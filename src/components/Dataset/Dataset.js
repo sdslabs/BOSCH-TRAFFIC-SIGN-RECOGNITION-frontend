7d901bf1055d6226277fc6cb68d0d52f224b41ca
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-03-11 19:50:55 +0530

    add react hooks

--- src/components/Dataset/Dataset.js
+++ src/components/Dataset/Dataset.js
@@ -1,36 +1,41 @@
 import React from 'react'
 import Sidebar from '../Common/Sidebar.js'
+// import structure from './structure.json'
 import { Container, Row, Col } from 'react-bootstrap'
 import { ReactComponent as UploadIcon } from '../../assets/images/upload.svg'
 import { ReactComponent as NewFolderIcon } from '../../assets/images/newfolder.svg'
 
-class Dataset extends React.Component {
-  render() {
-    return (
-      <Container fluid>
-        <Row className="mx-auto">
-          <Col xs={2.4}>
-            {' '}
-            <Sidebar />{' '}
-          </Col>
-          <Col>
-            <Row className="p-3">
-              <Col xs={2}>
-                <div className="primary-cta bw-10">
-                  <UploadIcon />
-                  Upload
-                </div>
-              </Col>
-              <Col className="p-auto my-auto">
-                <NewFolderIcon className="mr-3" />
-                New Folder
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+const Dataset = () => {
+  // structure = JSON.parse(structure)
+  // console.log(structure)
+
+  return (
+    <Container fluid>
+      <Row className="mx-auto">
+        <Col xs={2.4}>
+          {' '}
+          <Sidebar />{' '}
+        </Col>
+        <Col>
+          <Row className="py-3 border-bottom mx-0 px-0">
+            <Col xs={2}>
+              <div className="primary-cta bw-8">
+                <UploadIcon className="mr-3" />
+                Upload
+              </div>
+            </Col>
+            <Col className="p-auto my-auto">
+              <NewFolderIcon className="mr-3" />
+              New Folder
+            </Col>
+          </Row>
+          <Row>
+            <div className="">Select Dataset</div>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Dataset
---
+++ src/views/DatasetView.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Dataset from '../components/Dataset/Dataset.js'
+
+const DatasetView = () => {
+  return <Dataset />
+}
+
+export default DatasetView
