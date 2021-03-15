d56ca0836008bc26670e60e0ee51bcbfc096982c
Aviral Jain <avi.aviral140@gmail.com>
2021-03-15 05:45:38 +0530

    Fix css and design for dataset page1

--- src/components/Common/SidebarStep1.js
+++ src/components/Common/SidebarStep1.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import Tick from '../../assets/images/tick.svg'
 class Step1 extends React.Component {
   render() {
     return (
-      <Container fluid className="m-0 p-0">
+      <Container
+        fluid
+        className={'stepContainer ' + (this.props.done ? 'grey-bg' : '')}
+      >
         <Col>
-          <Row>
-            <h3>Step 1</h3>
+          <Row className="d-flex mb-2">
+            <div className="stepHeading">Step 1</div>
+            {this.props.done && <img src={Tick} className="mr-2" />}
           </Row>
           <Row>
-            <div>Select and generate dataset for model building</div>
+            <div className="stepInfo">
+              Select and generate dataset for model building
+            </div>
           </Row>
-          <Row className="d-flex justify-content-center mt-3">
-            <div className="primary-cta bw-10">Generate Dataset</div>
+          <Row className="d-flex justify-content-flex-start mt-3">
+            <button className="primary-cta primary-shadow">
+              Generate Dataset
+            </button>
           </Row>
         </Col>
       </Container>
--- src/components/Dataset/Dataset.js
+++ src/components/Dataset/Dataset.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../Common/Sidebar.js'
-// import DatasetPage1 from './DatasetPage1.js'
-import DatasetPage2 from './DatasetPage2/DatasetPage2'
+import DatasetPage1 from './DatasetPage1.js'
+// import DatasetPage2 from './DatasetPage2/DatasetPage2'
 // import structure from './structure.json'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Dataset = () => {
   // [TODO] Fetch structure here with an API call
-
+  const [datasetStep] = useState(1)
   return (
     <Container fluid className="h-100 mx-0 px-0">
       <Row className="mx-auto h-100">
         <Col xs={2.4}>
           {' '}
-          <Sidebar />{' '}
+          <Sidebar datasetStep={datasetStep} />{' '}
         </Col>
         <Col className="mx-0 px-0">
-          {/* <DatasetPage1 /> */}
-          <DatasetPage2 />
+          <DatasetPage1 />
+          {/* <DatasetPage2 /> */}
         </Col>
       </Row>
     </Container>
