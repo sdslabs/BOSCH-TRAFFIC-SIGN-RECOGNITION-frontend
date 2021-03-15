460abe1b28d36f32bcb108c6230546feb5c53381
Aviral Jain <avi.aviral140@gmail.com>
2021-03-15 16:59:12 +0530

    add page 3 for dataset

--- src/components/Dataset/Dataset.js
+++ src/components/Dataset/Dataset.js
@@ -3,7 +3,7 @@ import Sidebar from '../Common/Sidebar.js'
 import RightSidebar from '../Common/RightSidebar'
 import DatasetPage1 from './DatasetPage1.js'
 import DatasetPage2 from './DatasetPage2.js'
-import DatasetPage3 from './DatasetPage3/DatasetPage3'
+import DatasetPage3Data from './DatasetPage3/DatasetPage3Data'
 // import structure from './structure.json'
 import { Container, Row, Col } from 'react-bootstrap'
 import Step1 from '../Common/SidebarStep1.js'
@@ -13,7 +13,7 @@ const Dataset = () => {
   // [TODO] Fetch structure here with an API call
   const [datasetStep, setDatasetStep] = useState(1)
   const [initialData, setInitialData] = useState({ empty: true })
-  const [splitDataTraining, setSplitDataTraining] = useState(0)
+  const [splitDataTraining, setSplitDataTraining] = useState(70)
   return (
     <Container fluid className="h-100 mx-0 px-0">
       <Row className="mx-auto h-100">
@@ -49,7 +49,7 @@ const Dataset = () => {
           {datasetStep === 2 && (
             <DatasetPage2 setSplitDataTraining={setSplitDataTraining} />
           )}
-          {datasetStep === 3 && <DatasetPage3 />}
+          {datasetStep === 3 && <DatasetPage3Data />}
         </Col>
         <Col xs={2.4}>
           {' '}
---
+++ src/components/Dataset/DatasetPage3/DatasetPage3Data.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DatasetPage1 from '../DatasetPage1'
+
+const DatasetPage3Data = () => {
+  return (
+    <div className="dataset-page2 h-100">
+      <div className="data h-100">
+        <DatasetPage1 preview={true} isDataSplit={true} />
+      </div>
+    </div>
+  )
+}
+
+export default DatasetPage3Data
