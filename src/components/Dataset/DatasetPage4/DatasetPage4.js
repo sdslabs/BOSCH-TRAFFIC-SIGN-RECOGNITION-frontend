4b0d7bf97050a3a768767a446cce4f35fe6dd1f7
mhk19 <gmahak1@gmail.com>
2021-03-20 04:51:43 +0530

    add inside display of step 4

--- src/components/Dataset/DatasetPage4/DatasetPage4.js
+++ src/components/Dataset/DatasetPage4/DatasetPage4.js
@@ -2,8 +2,10 @@ import { Container, Col, Row } from 'react-bootstrap'
 import { Topbar } from './TopBar'
 import { InnerDisplay } from './InnerDisplay'
 import { RightSidebar } from './RightSideBar'
+import { useState } from 'react'
 
 const DatasetPage4 = props => {
+  const [selectedValues, setSelectedValues] = useState('')
   return (
     <Container fluid className="m-0 p-0">
       <Col className="m-0 p-0">
@@ -12,10 +14,16 @@ const DatasetPage4 = props => {
         </Row>
         <Row>
           <Col xs={7} className="border-right">
-            <InnerDisplay />
+            <InnerDisplay
+              selectedValues={selectedValues}
+              setSelectedValues={setSelectedValues}
+            />
           </Col>
           <Col xs={5} className="py-2">
-            <RightSidebar />
+            <RightSidebar
+              selectedValues={selectedValues}
+              setSelectedValues={setSelectedValues}
+            />
           </Col>
         </Row>
       </Col>
--- src/components/Dataset/DatasetPage4/InnerDisplay.js
+++ src/components/Dataset/DatasetPage4/InnerDisplay.js
@@ -1,5 +1,117 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Dropdown, DropdownButton } from 'react-bootstrap'
+import arrowdown4 from '../../../assets/images/arrowdown4.svg'
+import arrowdown from '../../../assets/images/arrowdown.svg'
+import arrowup from '../../../assets/images/arrowup.svg'
+import { useEffect, useState } from 'react'
+import plus from '../../../assets/images/plus.svg'
+import hiddenlayers from '../../../assets/images/hiddenlayers.svg'
+import trash from '../../../assets/images/trash.png'
 
 export const InnerDisplay = props => {
-  return <Container></Container>
+  const [nooflayers, setNoofLayers] = useState(0)
+  const [layers, setLayers] = useState([])
+  const increaseLayers = () => {
+    setNoofLayers(nooflayers + 1)
+  }
+  const decreaseLayers = () => {
+    nooflayers > 0 ? setNoofLayers(nooflayers - 1) : null
+  }
+  const addLayer = layer => {
+    layers.push({ name: layer, id: nooflayers })
+    increaseLayers()
+    console.log(props.selectedValues)
+    const newValues = { ...props.selectedValues }
+    newValues.layers = layers
+    props.setSelectedValues(newValues)
+    console.log(props.selectedValues)
+  }
+  const deleteLayer = id => {
+    for (let i = 0; i < layers.length; i++) {
+      if (layers[i].id == id) {
+        layers.splice(i, 1)
+        decreaseLayers()
+        const newValues = { ...props.selectedValues }
+        newValues.layers = layers
+        props.setSelectedValues(newValues)
+        console.log(props.selectedValues)
+        break
+      }
+    }
+  }
+  useEffect(() => {
+    console.log(layers)
+  }, [layers])
+  return (
+    <Container className="innerdisplay">
+      <h2> Hidden Layers </h2>
+      <Row className="upperlayer">
+        <Col className="image">
+          <img className="img-inner" src={hiddenlayers} alt="Image here" />
+        </Col>
+        <Col className="number-picker-outer">
+          <p className="num-pick-text">Number of Hidden layers</p>
+          <Row className="number-picker-inner">
+            <input className="num-pick-input" value={nooflayers} />
+            <Col className="num-pick-arrows">
+              <img
+                className="num-pick-down"
+                src={arrowup}
+                onClick={increaseLayers}
+              />
+              <img
+                className="num-pick-up"
+                src={arrowdown}
+                onClick={decreaseLayers}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Col className="lowerleft">
+        <Row className="middlelayer">
+          <img src={arrowdown4} />
+          <div className="input-text">Input</div>
+        </Row>
+        {layers.length != 0 ? (
+          <div className="layers">
+            {layers.map(layer => (
+              <div className="layer" key={layer.id}>
+                <div className="layer-text">{layer.name}</div>
+                <img
+                  className="trash"
+                  src={trash}
+                  onClick={() => {
+                    deleteLayer(layer.id)
+                  }}
+                />
+                <div />
+              </div>
+            ))}
+          </div>
+        ) : null}
+        <Row className="button">
+          <img className="plus" src={plus} />
+          <Dropdown>
+            <Dropdown.Toggle id="dropdown-basic" as="span">
+              Add layer
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => addLayer('Conv')}>
+                Conv
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => addLayer('Relu')}>
+                Relu
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => addLayer('Batch_Norm')}>
+                Batch_Norm
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Row>
+        <div className="const-btn">FC</div>
+        <div className="const-btn">FC</div>
+      </Col>
+      <Col className="lowerright"> Text here </Col>
+    </Container>
+  )
 }
