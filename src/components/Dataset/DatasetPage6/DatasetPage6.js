aedbae790935c67094211e68df5bb0f155bc81e3
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-03-22 02:13:16 +0530

    finish page 4

--- src/components/Dataset/DatasetPage6/DatasetPage6.js
+++ src/components/Dataset/DatasetPage6/DatasetPage6.js
@@ -1,14 +1,46 @@
+import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { getEmbedLink, getTensorflowExitLink } from '../../../api/datasetAPI'
 
 const DatasetPage6 = props => {
-  const getEmbedLink = async () => {}
+  const [tensorFlowLink, setTensorFlowLink] = useState()
+  useEffect(() => {
+    console.log('TensorFlow link updated: ', tensorFlowLink)
+    if (tensorFlowLink) {
+      checkIfExit()
+    }
+  }, [tensorFlowLink])
+
+  useEffect(() => {
+    handleGetLink()
+  }, [])
+
+  const handleGetLink = async () => {
+    const embedLink = await getEmbedLink()
+    setTensorFlowLink(embedLink.link)
+  }
+
+  const checkIfExit = async () => {
+    console.log('Checking if exit!')
+    setInterval(() => {
+      const res = getTensorflowExitLink()
+      if (res) {
+        res.then(resp => {
+          console.log('Tensorflow completed: ', resp.completed)
+          if (!(resp.completed === 'false')) {
+            props.setDatasetStep(5)
+          }
+        })
+      }
+    }, 5000)
+  }
+
   return (
     <Container className="m-0 p-0">
       <iframe
-        width="1672"
-        height="648"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={tensorFlowLink}
         frameBorder="0"
+        style={{ position: 'relative', height: '100%', width: '100' }}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen={false}
       ></iframe>
