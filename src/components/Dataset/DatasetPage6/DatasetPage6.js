7b5bb92bb737c9b5e26dd7da7c848445eed20931
Aviral Jain <avi.aviral140@gmail.com>
2021-03-25 07:46:22 +0530

    fix rendering of iframes merge conflict

--- src/components/Dataset/DatasetPage6/DatasetPage6.js
+++ src/components/Dataset/DatasetPage6/DatasetPage6.js
@@ -2,13 +2,6 @@ import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { getEmbedLink, getTensorflowExitLink } from '../../../api/datasetAPI'
 
-<<<<<<< Updated upstream
-const DatasetPage6 =()=> {
-  return (
-    <Container className="m-0 p-0 h-100" fluid>
-      <iframe
-        src={"http://localhost:6006"}
-=======
 const DatasetPage6 = props => {
   useEffect(()=>{
     waitFor5()
@@ -32,7 +25,6 @@ const DatasetPage6 = props => {
     <Container className="m-0 p-0 h-100" fluid id="iframecontainer">
       {/* <iframe
         src={props.tensorFlowLink}
->>>>>>> Stashed changes
         frameBorder="0"
         style={{ position: 'relative', height: '100%', width: '100%' }}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
