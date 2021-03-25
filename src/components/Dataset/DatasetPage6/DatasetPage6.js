4ba4644c1d0520509062a6d7082f0ec5ae7b010f
Aviral Jain <avi.aviral140@gmail.com>
2021-03-25 06:50:30 +0530

    Update DatasetPage6.js

--- src/components/Dataset/DatasetPage6/DatasetPage6.js
+++ src/components/Dataset/DatasetPage6/DatasetPage6.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { getEmbedLink, getTensorflowExitLink } from '../../../api/datasetAPI'
 
-const DatasetPage6 = props => {
+const DatasetPage6 =()=> {
   return (
     <Container className="m-0 p-0 h-100" fluid>
       <iframe
-        src={props.tensorFlowLink}
+        src={"http://localhost:6006"}
         frameBorder="0"
         style={{ position: 'relative', height: '100%', width: '100%' }}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
